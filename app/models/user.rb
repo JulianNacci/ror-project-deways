3cf515db2840551ed25f209a53c2da03193acfb1
cyril lopez <cyril.lpz@gmail.com>
2015-08-24 19:30:48 +0200

    add validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :address
+  validates_presence_of :phone_number
   has_many :bookings
   has_many :cars
 end
