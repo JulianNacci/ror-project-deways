af1232c1c38bc13c77000c801db8f499c6a28d70
Florent Allain <allain.florent@gmail.com>
2015-08-24 19:44:27 +0200

    added dashboard controller and sidebar layout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :cars, only: [:index, :show]
+  resources :dashboard, only: [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
