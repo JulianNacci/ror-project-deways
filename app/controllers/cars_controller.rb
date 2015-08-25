bd1737b2665d94d20482eef4ae890f544f227536
Julian Nacci <julian.nacci@gmail.com>
2015-08-25 11:55:21 +0200

    initial commit

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,6 +6,7 @@ class CarsController < ApplicationController
   end
 
   def show
+    @addresss = Address.all
   end
 
   def new
@@ -22,6 +23,7 @@ class CarsController < ApplicationController
 
   def create
     @car = Car.new(car_params)
+    @address = Address.new
     @car.save ? (redirect_to car_path(@car)) : (render 'new')
   end
 
