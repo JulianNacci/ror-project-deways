26f3a96b7261506c8beba2dd1f0fc1728b8f7fea
cyril lopez <cyril.lpz@gmail.com>
2015-08-25 12:02:55 +0200

    profil picture upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
+    styles: { medium: "300x300>", thumb: "100x100>" }, :default_url => "/images/:style/missing.png"
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
