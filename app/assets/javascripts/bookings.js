9a3614201ccd891101981ad4c96cdf2bf27083bc
Florent Allain <allain.florent@gmail.com>
2015-08-26 16:06:36 +0200

    change date format

--- app/assets/javascripts/bookings.js
+++ app/assets/javascripts/bookings.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+  $('.date').datepicker({ format: 'dd-mm-yyyy'});
+});
