6305cc7fb820310a5af8a2c6d66c27e920ff738c
Florent Allain <allain.florent@gmail.com>
2015-08-28 14:11:14 +0200

    got back to old datepicker

--- app/assets/javascripts/bookings.js
+++ app/assets/javascripts/bookings.js
@@ -1,14 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$( document ).ready(function() {
-  $('#datetimepicker1').datetimepicker();
-  $('#datetimepicker2').datetimepicker({
-    useCurrent: false
-  });
-  $("#datetimepicker1").on("dp.change", function (e) {
-    $('#datetimepicker2').data("DateTimePicker").minDate(e.date);
-  });
-  $("#datetimepicker2").on("dp.change", function (e) {
-    $('#datetimepicker1').data("DateTimePicker").maxDate(e.date);
-  });
-});
