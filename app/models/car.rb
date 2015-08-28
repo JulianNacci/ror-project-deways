b414eaee7f2fd66e184c4752098f49cafb5000be
Florent Allain <allain.florent@gmail.com>
2015-08-28 16:07:03 +0200

    solved booking strategy

--- app/models/car.rb
+++ app/models/car.rb
@@ -9,4 +9,33 @@ class Car < ActiveRecord::Base
   belongs_to :user
   has_many :addresses, dependent: :destroy
   has_many :bookings, dependent: :destroy
+
+
+
+
+  def available?(first_day, last_day)
+    output = true
+    if self.bookings
+      self.bookings.each do |booking|
+        booking_intervall(booking)
+        if (@booking_dates.include? first_day) || (@booking_dates.include? last_day)
+          output = false
+          break
+        end
+      end
+    end
+    output
+  end
+
+  private
+
+  def booking_intervall(booking)
+    @booking_dates = []
+    day = booking.checkin_date
+    while day <= booking.checkout_date do
+      @booking_dates << day
+      day += 1
+    end
+    return @booking_dates
+  end
 end
