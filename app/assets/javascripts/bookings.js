762117b276e1ed14558cb8ad574df3378d05b8b6
Florent Allain <allain.florent@gmail.com>
2015-08-28 11:44:42 +0200

    add js to datepicker

--- app/assets/javascripts/bookings.js
+++ app/assets/javascripts/bookings.js
@@ -1,13 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-$(document).ready(function(){
-  $('.datepicker').datepicker({
-    format: 'dd-mm-yyyy',
-    todayBtn: true,
-    clearBtn: true,
-    language: "fr",
-    autoclose: true,
-    todayHighlight: true,
-  });
-});
